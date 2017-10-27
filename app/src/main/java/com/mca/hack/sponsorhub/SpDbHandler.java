e38cb0ba1688e3da2cae654b4229be1fee2273a6
AhujaMayank <32630642+AhujaMayank@users.noreply.github.com>
2017-10-27 22:15:23 +0530

    username Password added

--- app/src/main/java/com/mca/hack/sponsorhub/SpDbHandler.java
+++ app/src/main/java/com/mca/hack/sponsorhub/SpDbHandler.java
@@ -16,6 +16,7 @@ public class SpDbHandler extends SQLiteOpenHelper {
     public final static String Sp_Ratio="Ratio";
     public final static String Sp_Logo="Logo";
     public final static String Sp_Amount="Amount";
+    public final static String Sp_Username="Username";
     public final static String Sp_Expectation="Expectation";
     public final static String Sp_Password="Password";
     public SpDbHandler(Context context){
@@ -31,6 +32,7 @@ public class SpDbHandler extends SQLiteOpenHelper {
                 Sp_Footfall + " text, " +
                 Sp_Contact + " text, " +
                 Sp_Password + "text" +
+                Sp_Username + "text" +
                 Sp_Logo + "BLOB NOT NULL" +
                 Sp_Ratio + " text);";
 
